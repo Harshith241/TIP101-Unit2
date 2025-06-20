6741354d094e88ee7d65240030944e91e796a7f7
Harshith241 <itzharshith.v06@gmail.com>
2025-06-20 20:33:21 +0530

    problem 6 done!! of v1

---
+++ Session1/ProblemSet-V1/Problem6.py
@@ -0,0 +1,37 @@
+'''Write a function calculate_gpa() that calculates the grade point average (GPA) for a student based on their course grades and returns the gpa as a float. The function takes in a dictionary report_card as a parameter where each key-value pair represents a course and the grade received in that course respectively. The grades are represented as strings ("A", "B", "C", "D", "F") and each grade corresponds to a certain number of grade points:
+
+"A" = 4
+"B" = 3
+"C" = 2
+"D" = 1
+"F" = 0
+
+A GPA is calculated by finding the average of all grade points.
+
+def calculate_gpa(report_card):
+    pass
+Example Usage:
+
+report_card = {"Math": "A", "Science": "C", "History": "A", "Art": "B", "English": "B", "Spanish": "A"}
+print(calculate_gpa(report_card))
+Example Output: 3.33
+
+'''
+
+def calculate_gpa(report_card):
+    just_values = report_card.values()
+    sum = 0
+    for grade in just_values:
+        if grade == "A":
+            sum += 4
+        elif grade == "B":
+            sum += 3
+        elif grade == "C":
+            sum += 2
+        elif grade == "D":
+            sum += 1
+    GPA = sum/len(report_card)
+    return round(GPA, 2)
+
+report_card = {"Math": "A", "Science": "C", "History": "A", "Art": "B", "English": "B", "Spanish": "A"}
+print(calculate_gpa(report_card))
